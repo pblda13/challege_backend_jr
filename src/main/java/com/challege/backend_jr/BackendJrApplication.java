0dd0b35d496405d2dcd654b871f427d99dec2588
Pâmela Borges <pamela_borges18@hotmail.com>
2024-04-18 12:50:54 -0300

    Starting the project

---
+++ src/main/java/com/challege/backend_jr/BackendJrApplication.java
@@ -0,0 +1,13 @@
+package com.challege.backend_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJrApplication.class, args);
+	}
+
+}
